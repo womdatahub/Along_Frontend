6312574e35d672d83e6c0b4c3b4abeab97311735
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2025-09-14 17:35:47 +0100

    chore: rent-ride component

---
+++ src/app/rent-ride/page.tsx
@@ -0,0 +1,125 @@
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+  HeadingHeebo,
+} from "@/components";
+import { cn } from "@/lib";
+import { AccuracyIcon, WhiteForwardIcon } from "@public/svgs";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import Image from "next/image";
+
+const Page = () => {
+  return (
+    <div className='px-4 md:px-0 max-w-7xl mx-auto w-full flex- py-8 md:py-14 h-[calc(100vh-80px)] overflow-hidden'>
+      <div className='flex gap-4'>
+        <div className='flex flex-col gap-10 w-fit'>
+          <div className='flex flex-col'>
+            <HeadingHeebo className='text-left font-extrabold text-4xl'>
+              Rent a ride
+            </HeadingHeebo>
+            <p className='text-sm'>
+              To proceed, enter your pick up location to see <br /> available
+              cars
+            </p>
+          </div>
+          <div className='flex items-center gap-8'>
+            <div className='flex gap-4 items-center px-4 py-3 bg-white rounded-2xl'>
+              <AccuracyIcon />
+              <input
+                className={cn(
+                  "text-sm focus:outline-none focus:ring-0 placeholder:text-placeholder w-full md:w-[375px]"
+                )}
+                placeholder='Pick up location'
+              />
+            </div>
+
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  variant={"default"}
+                  className='bg-transparent hover:bg-transparent shadow-none border-none cursor-pointer flex items-center gap-3 px-0'
+                >
+                  <div className='bg-primary rounded-full size-10 flex items-center justify-center'>
+                    <WhiteForwardIcon />
+                  </div>
+                </Button>
+              </DialogTrigger>
+              <DialogContent
+                className='sm:max-w-[425px] px-4 py-8 rounded-[20px] bg-background-1'
+                showCloseButton={false}
+              >
+                <VisuallyHidden>
+                  <DialogTitle>
+                    Select a vehicle type: Economy, Comfort, Comfort XL, Luxury
+                    or Luxury XL
+                  </DialogTitle>
+                </VisuallyHidden>
+                <div className='flex flex-col gap-6'>
+                  <div className='flex flex-col pl-7'>
+                    <HeadingHeebo className='text-primary font-semibold text-xl text-left'>
+                      Vehicle type
+                    </HeadingHeebo>
+                    <p className='text-sm'>
+                      Please select a vehicle option to continue <br /> your
+                      booking
+                    </p>
+                  </div>
+                  <div className='flex flex-col gap-1'>
+                    {items.map((i) => {
+                      return (
+                        <div
+                          key={i.name}
+                          className='flex gap-4 items-center rounded-lg bg-white px-4 h-[71px] hover:bg-primary/70 cursor-pointer group transition-colors duration-150'
+                        >
+                          <Image
+                            src={"/images/small-car.png"}
+                            alt={"car"}
+                            width={40}
+                            height={40}
+                          />
+                          <div className='flex flex-col group-hover:text-white duration-150'>
+                            <p className='font-semibold text-sm'>{i.name}</p>
+                            <p className='text-xs'>{i.seat} Persons</p>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </div>
+
+        <div className='bg-red-400 w-full h-full min-h-40'></div>
+      </div>
+    </div>
+  );
+};
+export default Page;
+
+const items = [
+  {
+    name: "Economy",
+    seat: 4,
+  },
+  {
+    name: "Comfort",
+    seat: 4,
+  },
+  {
+    name: "Comfort XL",
+    seat: 4,
+  },
+  {
+    name: "Luxury",
+    seat: 4,
+  },
+  {
+    name: "Luxury XL",
+    seat: 6,
+  },
+];
