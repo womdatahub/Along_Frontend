f1dd3053bb4818c4127b068b5db42425594e00a6
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2025-09-15 21:13:51 +0100

    chore: window size

---
+++ src/components/shared/check-window-size.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export const CheckWindowSize = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const check = () => setIsMobile(window.innerWidth < 650);
+    check();
+    window.addEventListener("resize", check);
+    return () => window.removeEventListener("resize", check);
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div className='flex items-center justify-center h-screen w-screen'>
+        <p className='text-lg font-semibold text-center px-4'>
+          To use this service on mobile, please download our app.
+        </p>
+      </div>
+    );
+  }
+
+  return <>{children}</>;
+};
