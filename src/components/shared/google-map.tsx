9545ee9b6bd807a19938b8431413e798d095a54b
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2025-09-19 12:09:20 +0100

    chore: google map

---
+++ src/components/shared/google-map.tsx
@@ -0,0 +1,60 @@
+"use client";
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+} from "@react-google-maps/api";
+
+const center = {
+  lat: 37.7749,
+  lng: -122.4194,
+};
+
+/*
+from defaults to center if not given
+To
+Markers [
+  { lat: 37.7749, lng: -122.4194 },
+  { lat: 37.7749, lng: -122.4194 },]
+
+*/
+export const GoogleMaps = () => {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
+    libraries: ["places"],
+  });
+
+  if (!isLoaded) {
+    return <div>GoogleMaps is instantiating</div>;
+  }
+
+  return (
+    <div className='w-full h-full'>
+      <GoogleMap
+        center={center}
+        zoom={15}
+        mapContainerStyle={{
+          width: "100%",
+          height: "100%",
+        }}
+        options={{
+          zoomControl: false,
+          streetViewControl: false,
+          mapTypeControl: false,
+          fullscreenControl: false,
+        }}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </div>
+  );
+};
+
+export const GoogleMapAutoComplete = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return <Autocomplete>{children}</Autocomplete>;
+};
