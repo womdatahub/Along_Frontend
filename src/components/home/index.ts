805de1067b7efcf37eea873fbea615ba8f3c7b6a
womdatahub <womdatahub@gmail.com>
2025-09-22 19:29:54 +0100

    Update index.ts

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,9 +1,9 @@
-export { default as Navbar } from "./nav-bar";
-export { default as Hero } from "./hero";
-export { default as Features } from "./features";
-export { default as Services } from "./services";
-export { default as HowItWorks } from "./how-it-works";
-export { default as AppDownload } from "./app-download";
-export { default as Faq } from "./faq";
-export { default as Updates } from "./updates";
-export { default as Footer } from "./footer";
+export { Navbar } from "./nav-bar";
+export { Hero } from "./hero";          // after fixing Hero syntax, ensure it's named or default, see note below
+export { Features } from "./features";
+export { Services } from "./services";
+export { HowItWorks } from "./how-it-works";
+export { AppDownload } from "./app-download";
+export { Faq } from "./faq";
+export { Updates } from "./updates";
+export { Footer } from "./footer";
