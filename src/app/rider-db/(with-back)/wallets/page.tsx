80ca3ad5c89374845ddd59a523ae6c8e083977b1
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2025-10-16 20:16:35 +0100

    chore: wallet redesign chore

--- src/app/rider-db/(with-back)/wallets/page.tsx
+++ src/app/rider-db/(with-back)/wallets/page.tsx
@@ -1,36 +1,27 @@
 "use client";
-import {
-  Button,
-  Card,
-  CardContent,
-  CardFooter,
-  HeadingHeebo,
-} from "@/components";
+import { Button, Card, CardContent, HeadingHeebo } from "@/components";
 
 const Page = () => {
   return (
     <div className='flex flex-col gap-5'>
       <HeadingHeebo className='text-start pl-4'>Wallet</HeadingHeebo>
       <Card className='w-full md:w-[446px] rounded-2xl shadow-none'>
-        <CardContent className='flex flex-col gap-12'>
+        <CardContent className='flex flex-col gap-12 p-0 items-center'>
           <div className='flex flex-col gap-1 font-heebo'>
-            <p className='text-base'>Balance</p>
+            <p className='text-base text-center'>Balance</p>
             <p className='font-semibold font-heebo text-4xl'>$4652.98</p>
           </div>
         </CardContent>
-        <CardFooter>
-          <div className='flex justify-between items-center gap-8'>
-            <Button className='cursor-pointer text-sm rounded-2xl h-11 bg-gray-2 text-black hover:bg-gray-2/90'>
-              Credit with debit card
-            </Button>
-            <Button
-              variant={"ghost"}
-              className='cursor-pointer hover:bg-transparent p-0 h-fit'
-            >
-              Bank transfer
-            </Button>
-          </div>
-        </CardFooter>
+        <div className='text-base h-11 bg-gray-2 text-black flex gap-3 items-center justify-center'>
+          <p>Bonus Bal:</p>
+          <p className='font-heebo font-medium text-2xl'>$98</p>
+        </div>
+        <Button
+          variant='default'
+          className='cursor-pointer self-center rounded-xl h-11'
+        >
+          Credit with debit card
+        </Button>
       </Card>
     </div>
   );
