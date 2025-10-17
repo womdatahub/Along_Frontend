2ee2c1ad92c2831f0a7c0f74269e376c978b78b7
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2025-10-17 22:44:11 +0100

    chore: implement user session management with fetchUserDetails in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,28 @@ import {
   Updates,
   Footer,
 } from "@/components";
+import { useSession } from "@/store";
+import { useEffect } from "react";
 
 export default function Home() {
+  const {
+    actions: { login, fetchUserDetails },
+  } = useSession((state) => state);
+
+  useEffect(() => {
+    fetchUserDetails()
+      // login({
+      //   email: "javascriptwonder@gmail.com",
+      //   password: "se3di$God",
+      // })
+      .finally(() => {
+        console.log("user details fetched in");
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log("RENDERING HOME PAGE");
   return (
     <div className='font-fustat overflow-x-hidden'>
       <Navbar />
