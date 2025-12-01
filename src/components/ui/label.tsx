04b21a0a25c15ae488f5e74368cebb2cdd5baabc
wondersmasher <wonderfulakinniyi@gmail.com>
2025-12-01 01:18:40 -0800

    chore: shadcn sb

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
 
-import { cn } from "@/lib";
+import { cn } from "@/lib/index";
 
 function Label({
   className,
