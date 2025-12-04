10575fc01112a7ec49ddae5a18fc2ca5f3177ac4
wondersmasher <wonderfulakinniyi@gmail.com>
2025-12-04 06:58:44 -0800

    chore: update admin SVG icons and add new admin pages

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -1,79 +1,85 @@
 "use client";
 
 import { SidebarMenu, SidebarMenuButton } from "@/components/ui/sidebar";
-
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib";
-import { AdminDashboardIcon } from "@public/svgs";
-
-export const menuItems = [
-  {
-    icon: <AdminDashboardIcon isActive={false} />,
-    text: "Dashboard",
-    path: "/dashboard",
-  },
-  {
-    icon: <AdminDashboardIcon isActive />,
-    text: "User Management",
-    path: "/dashboard/user-management",
-  },
-  // {
-  //   text: "Case Management",
-  //   path: "/dashboard/case-management",
-  // },
-  // {
-  //   text: "Veterinary Directory",
-  //   path: "/dashboard/veterinary-directory",
-  // },
-  // { text: "Report", path: "/dashboard/report" },
-  // {
-  //   text: "Agro Businesses",
-  //   path: "/dashboard/agro-businesses",
-  // },
-  // {
-  //   text: "Account Approvals",
-  //   path: "/dashboard/account-approvals",
-  // },
-  // { text: "Community", path: "/dashboard/community" },
-  // {
-  //   text: "Audit Logs",
-  //   path: "/dashboard/audit-logs",
-  // },
-  // {
-  //   text: "Feedback & Reviews",
-  //   path: "/dashboard/feedback-and-reviews",
-  // },
-];
+import {
+  AdminDashboardIcon,
+  AdminRiderssIcon,
+  AdminDriversAndFleetsIcon,
+  AdminRulesAndPermissionsIcon,
+} from "@public/svgs";
+import { JSX, SVGProps, useState } from "react";
 
 export const NavMain = () => {
-  const pathname = usePathname();
-
   return (
     <SidebarMenu className="mt-6 px-4">
-      {menuItems.map((item, index) => {
-        const isActive = (() => {
-          if (item.path === "/dashboard") {
-            return pathname === "/dashboard";
-          }
-          return pathname === item.path || pathname.startsWith(item.path + "/");
-        })();
-        return (
-          <SidebarMenuButton
-            key={index}
-            className={cn(
-              "hover:bg-white text-white hover:text-primary h-12 rounded-full px-8",
-              isActive && "bg-white text-primary"
-            )}
-            asChild
-          >
-            <Link href={item.path}>
-              {item.icon}
-              <span className="text-[15px]">{item.text}</span>
-            </Link>
-          </SidebarMenuButton>
-        );
+      {menuItems.map(({ icon: Icon, path, text }, index) => {
+        return <SMenuButton key={index} Icon={Icon} path={path} text={text} />;
       })}
     </SidebarMenu>
   );
 };
+
+type SMenuButtonProps = {
+  path: string;
+  key: number;
+  Icon: (props: SVGProps<SVGElement>) => JSX.Element;
+  text: string;
+};
+const SMenuButton = ({ path, key, Icon, text }: SMenuButtonProps) => {
+  const pathname = usePathname();
+  let isActive = (() => {
+    if (path === "/admin") {
+      return pathname === "/admin";
+    }
+    return pathname === path || pathname.startsWith(path + "/");
+  })();
+  const [hover, setHover] = useState(false);
+  isActive = isActive || hover;
+
+  return (
+    <SidebarMenuButton
+      key={key}
+      className={cn(
+        "group hover:bg-white text-white hover:text-primary h-12 rounded-full px-8",
+        isActive && "bg-white text-primary"
+      )}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      asChild
+    >
+      <Link href={path}>
+        <Icon
+          fill={isActive ? "#0E696A" : "white"}
+          className="group-hover:fill-[#0E696A]"
+        />
+        <span className="text-[15px]">{text}</span>
+      </Link>
+    </SidebarMenuButton>
+  );
+};
+
+const menuItems = [
+  {
+    icon: AdminDashboardIcon,
+    text: "Dashboard",
+    path: "/admin",
+  },
+  {
+    icon: AdminDriversAndFleetsIcon,
+    text: "Drivers & Fleets",
+    path: "/admin/drivers-and-fleets",
+  },
+  {
+    icon: AdminRiderssIcon,
+    text: "Riders",
+    path: "/admin/riders",
+  },
+  {
+    icon: AdminRulesAndPermissionsIcon,
+    text: "Roles and permission",
+    path: "/admin/roles-and-permission",
+  },
+];
