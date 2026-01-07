e6e239b4ff26f7a21db9ba24cfeacee1276b9991
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2026-01-07 16:34:00 +0100

    feat: enhance app download and how it works components for better responsiveness and readability

--- src/components/home/app-download.tsx
+++ src/components/home/app-download.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Button, HeadingHeebo } from "@/components";
+import { useIsMobile } from "@/hooks/use-mobile";
 import {
   // AndroidWhiteIcon, IOSWhiteIcon,
   WhiteForwardIcon,
@@ -6,11 +8,12 @@ import {
 import Image from "next/image";
 
 export const AppDownload = () => {
+  const isMobile = useIsMobile();
   return (
     <section className='px-6 py-4 bg-primary text-white font-heebo'>
       <div className='flex items-center gap-10 max-w-6xl mx-auto'>
         <div className='flex flex-col gap-2 md:w-1/2'>
-          <HeadingHeebo className='text-left font-extrabold text-4xl'>
+          <HeadingHeebo className='text-left font-extrabold text-2xl md:text-4xl'>
             Get more from the app
           </HeadingHeebo>
           <p className='text-lg font-light'>
@@ -22,28 +25,26 @@ export const AppDownload = () => {
             className='flex
            gap-2 md:gap-4'
           >
-            <Button className='border-white border-2 flex items-center gap-4 justify-between bg-transparent hover:bg-transparent font-semibold text-2xl h-20 rounded-3xl'>
-              <div className='flex gap-4 items-center'>
+            <Button className='border-white border-2 flex items-center gap-2 md:gap-4 justify-between bg-transparent hover:bg-transparent font-semibold text-xl md:text-2xl rounded-xl md:rounded-3xl py-8'>
+              <div className='flex gap-2 md:gap-4 items-center'>
                 <Image
                   src={"/images/android.png"}
                   alt='app-download'
-                  width={38}
-                  height={44}
+                  width={isMobile ? 20 : 38}
+                  height={isMobile ? 20 : 38}
                 />
-                {/* <AndroidWhiteIcon width={60} height={60} viewBox='0 0 60 60' /> */}
                 Android
               </div>
               <WhiteForwardIcon />
             </Button>
-            <Button className='border-white border-2 flex items-center gap-4 justify-between bg-transparent hover:bg-transparent font-semibold text-2xl h-20 rounded-3xl'>
-              <div className='flex gap-4 items-center'>
+            <Button className='border-white border-2 flex items-center gap-2 md:gap-4 justify-between bg-transparent hover:bg-transparent font-semibold text-xl md:text-2xl rounded-xl md:rounded-3xl py-8'>
+              <div className='flex gap-2 md:gap-4 items-center'>
                 <Image
                   src={"/images/ios.png"}
                   alt='app-download'
-                  width={38}
-                  height={44}
+                  width={isMobile ? 20 : 38}
+                  height={isMobile ? 20 : 38}
                 />
-                {/* <IOSWhiteIcon width={60} height={60} viewBox='0 0 60 60' /> */}
                 Apple
               </div>
               <WhiteForwardIcon />
