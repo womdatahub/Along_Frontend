13957bf3975a5cfa9232a3974daeab0a2304aef2
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2026-01-07 16:32:35 +0100

    feat: add active trip page with trip details and locations

---
+++ src/app/(admin-pages)/admin/active-trip/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { Card, CardContent } from "@/components";
+
+const Page = () => {
+  return (
+    <section className='flex flex-col gap-8'>
+      <p className='text-4xl font-heebo'>Active Trip</p>
+      <Card className='w-full md:w-[422px] rounded-2xl shadow-none border-0'>
+        <CardContent className='flex flex-col gap-3 px-0'>
+          <div className='font-heebo flex justify-between items-center font-semibold text-sm text-primary px-4'>
+            <p>Scheduled Trip</p>
+            <p className='text-black text-xs'>Trip ID: 109934</p>
+          </div>
+          <div className='flex flex-col gap-1 px-6'>
+            <div className='flex gap-2'>
+              <div className='flex self-end'>
+                <svg
+                  width='15'
+                  height='15'
+                  viewBox='0 0 15 15'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    d='M7.5 0C7.88463 0 8.20163 0.28953 8.24495 0.662534L8.25 0.75L8.25026 1.73547C10.8607 2.07188 12.9286 4.13999 13.2646 6.7505L14.25 6.75C14.6642 6.75 15 7.08579 15 7.5C15 7.88463 14.7105 8.20163 14.3375 8.24495L14.25 8.25L13.2645 8.25026C12.9282 10.8604 10.8604 12.9282 8.25026 13.2645L8.25 14.25C8.25 14.6642 7.91421 15 7.5 15C7.11537 15 6.79837 14.7105 6.75505 14.3375L6.75 14.25L6.7505 13.2646C4.13999 12.9286 2.07188 10.8607 1.73547 8.25026L0.75 8.25C0.335786 8.25 0 7.91421 0 7.5C0 7.11537 0.28953 6.79837 0.662534 6.75505L0.75 6.75L1.73538 6.7505C2.07148 4.13974 4.13974 2.07148 6.7505 1.73538L6.75 0.75C6.75 0.335786 7.08579 0 7.5 0ZM7.5 3.1875C5.11827 3.1875 3.1875 5.11827 3.1875 7.5C3.1875 9.88173 5.11827 11.8125 7.5 11.8125C9.88173 11.8125 11.8125 9.88173 11.8125 7.5C11.8125 5.11827 9.88173 3.1875 7.5 3.1875ZM7.5 4.5C9.15685 4.5 10.5 5.84315 10.5 7.5C10.5 9.15685 9.15685 10.5 7.5 10.5C5.84315 10.5 4.5 9.15685 4.5 7.5C4.5 5.84315 5.84315 4.5 7.5 4.5Z'
+                    fill='black'
+                  />
+                </svg>
+              </div>
+
+              <div className='font-fustat'>
+                <p className='font-light text-[10px] text-gray-5'>
+                  Pick up location
+                </p>
+                <p className='font-bold text-xs'>
+                  Mountain Hills Boulevard, Texas
+                </p>
+              </div>
+            </div>
+            <div className='w-[1px] h-4 bg-[#3F3F3F] ml-[7px]' />
+            <div className='flex gap-2'>
+              <div className='flex self-end'>
+                <svg
+                  width='15'
+                  height='15'
+                  viewBox='0 0 15 15'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <rect width='13' height='13' rx='2' fill='black' />
+                </svg>
+              </div>
+
+              <div className='font-fustat -mt-[14px]'>
+                <p className='font-light text-[10px] text-gray-5'>
+                  Drop off location
+                </p>
+                <p className='font-bold text-xs'>St Louise Avenue, Florida</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex gap-4 justify-between items-center px-4 mt-6 pt-4 border-t-2'>
+            <p className='text-[10px] text-gray-5'>Vehicle type</p>
+            <p className='font-heebo font-semibold text-xs'>Economy</p>
+          </div>
+        </CardContent>
+      </Card>
+    </section>
+  );
+};
+export default Page;
--- src/app/(admin-pages)/admin/page.tsx
+++ src/app/(admin-pages)/admin/page.tsx
@@ -1,112 +1,115 @@
 "use client";
 
-import { Card, CardContent, CardTitle } from "@/components/";
+import { Button, Card, CardContent, CardTitle } from "@/components/";
 import { cn } from "@/lib";
 
 const Page = () => {
   return (
-    <section className="flex flex-col gap-8">
-      <p className="text-4xl font-heebo">Dashboard</p>
-      <div className="flex gap-8">
-        <div className="flex gap-8 flex-col w-[33vw]">
-          <Card className="px-4 py-7">
-            <CardTitle className="p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]">
+    <section className='flex flex-col gap-8'>
+      <p className='text-4xl font-heebo'>Dashboard</p>
+      <div className='flex gap-8'>
+        <div className='flex gap-8 flex-col w-[33vw]'>
+          <Card className='px-4 py-7'>
+            <CardTitle className='p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]'>
               Queues
             </CardTitle>
-            <CardContent className="flex flex-col gap-6 px-0">
+            <CardContent className='flex flex-col gap-6 px-0'>
               {queues.map((q) => (
-                <div key={q.text} className="flex gap-4 items-center">
+                <div key={q.text} className='flex gap-4 items-center'>
                   <div
-                    className={cn(
-                      "w-[5.5vw] flex justify-center items-center py-0.5 font-medium rounded-full",
-                      `${q.badgeColor} ${q.textColor}`
-                    )}
+                    className={cn("w-[5.5vw] font-medium", `${q.textColor}`)}
                   >
                     {q.title}
                   </div>
-                  <div className="font-semibold text-sm">{q.text}</div>
+                  <div className='font-semibold text-sm'>{q.text}</div>
                 </div>
               ))}
             </CardContent>
           </Card>
-          <Card className="px-4 py-7">
-            <CardTitle className="p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]">
+          <Card className='px-4 py-7'>
+            <CardTitle className='p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]'>
               Live Operations
             </CardTitle>
-            <CardContent className="flex flex-col px-0">
+            <CardContent className='flex flex-col px-0'>
               {liveOperations.map((live, i) => (
                 <div
-                  className="flex items-center border-b last:border-b-0 p-4"
+                  className='flex items-center border-b last:border-b-0 p-4'
                   key={i}
                 >
-                  <div className="flex flex-col gap-1 font-heebo w-1/4">
-                    <p className="text-[#768B8F] text-[8px]">Type</p>
-                    <p className="text-black text-[9px]">{live.type}</p>
+                  <div className='flex flex-col gap-1 font-heebo w-1/4'>
+                    <p className='text-[#768B8F] text-[8px]'>Type</p>
+                    <p className='text-black text-[9px]'>{live.type}</p>
                   </div>
-                  <div className="flex flex-col gap-1 font-heebo w-1/4">
-                    <p className="text-[#768B8F] text-[8px]">Customer</p>
-                    <p className="text-black text-[9px]">{live.customer}</p>
+                  <div className='flex flex-col gap-1 font-heebo w-1/4'>
+                    <p className='text-[#768B8F] text-[8px]'>Customer</p>
+                    <p className='text-black text-[9px]'>{live.customer}</p>
                   </div>
-                  <div className="flex flex-col gap-1 font-heebo w-1/4">
-                    <p className="text-[#768B8F] text-[8px]">Driver</p>
-                    <p className="text-black text-[9px]">{live.driver}</p>
+                  <div className='flex flex-col gap-1 font-heebo w-1/4'>
+                    <p className='text-[#768B8F] text-[8px]'>Driver</p>
+                    <p className='text-black text-[9px]'>{live.driver}</p>
                   </div>
                 </div>
               ))}
             </CardContent>
           </Card>
         </div>
-        <div className="flex gap-8 flex-col flex-1">
-          <Card className="px-4 py-7 gap-1 ">
-            <CardTitle className="p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]">
+        <div className='flex gap-8 flex-col flex-1'>
+          <Card className='px-4 py-7 gap-1 '>
+            <CardTitle className='p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]'>
               Live heatmap
             </CardTitle>
-            <CardContent className="px-0 h-[250px]"></CardContent>
+            <CardContent className='px-0 h-[250px]'></CardContent>
           </Card>
-          <div className="grid grid-cols-2 gap-6 flex-wrap">
-            <Card className="px-4 py-7 gap-1">
-              <CardTitle className="p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]">
+          <div className='grid grid-cols-2 gap-6 flex-wrap'>
+            <Card className='px-4 py-7 gap-1'>
+              <CardTitle className='p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]'>
                 Active trips
               </CardTitle>
-              <CardContent className="px-0">
-                <p className="font-bold text-4xl">3,550</p>
+              <CardContent className='px-0'>
+                <p className='font-bold text-4xl'>3,550</p>
               </CardContent>
             </Card>
-            <Card className="px-4 py-7 gap-1">
-              <CardTitle className="p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]">
+            <Card className='px-4 py-7 gap-1'>
+              <CardTitle className='p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]'>
                 Scheduled Trips
               </CardTitle>
-              <CardContent className="px-0">
-                <p className="font-bold text-4xl">320</p>
+              <CardContent className='px-0'>
+                <p className='font-bold text-4xl'>320</p>
               </CardContent>
             </Card>
-            <Card className="px-4 py-7 gap-1">
-              <CardTitle className="p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]">
+            <Card className='px-4 py-7 gap-1'>
+              <CardTitle className='p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]'>
                 Rush SLA Met
               </CardTitle>
-              <CardContent className="px-0">
-                <p className="font-bold text-4xl">78%</p>
+              <CardContent className='px-0'>
+                <p className='font-bold text-4xl'>78%</p>
               </CardContent>
             </Card>
-            <Card className="px-4 py-7 gap-1 ">
-              <CardTitle className="p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]">
+            <Card className='px-4 py-7 gap-1 '>
+              <CardTitle className='p-0 m-0 text-left font-heebo font-medium text-xl text-[#768B8F]'>
                 Open SOS
               </CardTitle>
-              <CardContent className="px-0">
-                <p className="font-bold text-4xl">14</p>
+              <CardContent className='px-0'>
+                <p className='font-bold text-4xl'>14</p>
               </CardContent>
             </Card>
           </div>
-          <Card className="px-4 py-7 bg-[#768B8F] text-white">
-            <CardTitle className="p-0 m-0 text-left font-heebo font-medium text-xl text-white">
+          <Card className='px-4 py-7 bg-[#768B8F] text-white'>
+            <CardTitle className='p-0 m-0 text-left font-heebo font-medium text-xl text-white'>
               Ad Notification
             </CardTitle>
-            <CardContent className="px-0 flex flex-col gap-2">
+            <CardContent className='px-0 flex flex-col gap-2'>
               {notifications.map((notification) => (
-                <p key={notification} className="">
+                <p key={notification} className=''>
                   {notification}
                 </p>
               ))}
+              <Button
+                variant='outline'
+                className='border-white rounded-full bg-transparent mt-4'
+              >
+                Edit entry
+              </Button>
             </CardContent>
           </Card>
         </div>
