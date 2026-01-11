47d5c34fa02f54ce845626697f07eab141dbbd61
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2026-01-11 21:53:12 +0100

    fix: refactor vehicle-info page to use AddInput component and improve form validation

--- src/app/(auth)/onboarding/(driver)/vehicle-info/page.tsx
+++ src/app/(auth)/onboarding/(driver)/vehicle-info/page.tsx
@@ -1,19 +1,76 @@
 "use client";
 
-import { AuthBackAndContinueButton, CustomAuthInput } from "@/components";
+import { AddInput, AuthBackAndContinueButton } from "@/components";
 import { HeadingHeebo } from "@/components";
 import { UploadingImagesReusableComponent } from "@/components/shared/uploading-images-reusable-component";
+import {
+  TSocialSecurityNumberSchemaValidator,
+  TVehicleRegistrationSchemaValidator,
+  socialSecurityNumberSchema,
+  vehicleRegistrationSchema,
+} from "@/lib";
+import { useSession } from "@/store";
 import { ImageType } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { UploadImageIcon } from "@public/svgs";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 const Page = () => {
   const [previews, setPreviews] = useState<
     ({ image: ImageType; uri: string } | null)[]
   >([null, null, null, null, null]);
 
+  const [imagesUri, setImagesUri] = useState<string[]>([]);
+  const {
+    actions: { uploadImages, registerVehicle },
+  } = useSession((state) => state);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TVehicleRegistrationSchemaValidator>({
+    defaultValues: {
+      vehicleMake: "",
+      vehicleModel: "",
+      vehicleColor: "",
+      vehicleIdentificationNumber: "",
+      vehicleYear: "",
+    },
+    resolver: zodResolver(vehicleRegistrationSchema),
+  });
+
   const router = useRouter();
+
+  const onSubmit = async (v: TVehicleRegistrationSchemaValidator) => {
+    console.log(v, errors);
+    if (previews.includes(null)) {
+      toast.error("All images are required");
+      return;
+    }
+
+    for (let i = 0; i < previews.length; i++) {
+      const uri = await uploadImages({
+        uploadType: "vehicle",
+        imageFile: previews[i]?.image.imageFile as ImageType["imageFile"],
+      });
+      if (!uri) return;
+      setImagesUri((prev) => [...prev, uri]);
+    }
+
+    const isSuccess = await registerVehicle({
+      ...v,
+      vehicleFrontViewImageUri: imagesUri[0],
+      vehicleBackViewImageUri: imagesUri[1],
+      vehicleSideViewImageUri: imagesUri[2],
+      insuranceDocumentUri: imagesUri[3],
+    });
+    if (!isSuccess) return;
+    router.push("/onboarding/vehicle-insurance");
+  };
   return (
     <div className='flex flex-col gap-10 rounded-[20px] w-[500px] px-8 py-10 bg-background-1 text-black'>
       <div className='flex flex-col gap-2'>
@@ -24,19 +81,67 @@ const Page = () => {
         </p>
       </div>
       <div className='flex flex-col gap-8'>
-        <CustomAuthInput label='Car make' placeholder='Tesla' />
-        <CustomAuthInput label='Car model' placeholder='Model Y' />
-        <CustomAuthInput
+        <AddInput
+          label='Car make'
+          placeholder='Tesla'
+          id='vehicleMake'
+          errors={errors}
+          register={register}
+          disabled={false}
+          required
+          type='text'
+          iconAndInputWrapperClassName='bg-white rounded-2xl h-16'
+          inputClassName='placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0  shadow-none'
+        />
+        <AddInput
+          label='Car model'
+          placeholder='Model Y'
+          id='vehicleModel'
+          errors={errors}
+          register={register}
+          disabled={false}
+          required
+          type='text'
+          iconAndInputWrapperClassName='bg-white rounded-2xl h-16'
+          inputClassName='placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0  shadow-none'
+        />
+        <AddInput
           label='Car ID number'
           placeholder='0000000000000000000'
+          id='vehicleIdentificationNumber'
+          errors={errors}
+          register={register}
+          disabled={false}
+          required
+          type='text'
+          iconAndInputWrapperClassName='bg-white rounded-2xl h-16'
+          inputClassName='placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0  shadow-none'
         />
         <div className='flex gap-4'>
-          <CustomAuthInput
+          <AddInput
             label='Car color'
             placeholder='Beige white'
-            className='flex-1'
+            id='vehicleColor'
+            errors={errors}
+            register={register}
+            disabled={false}
+            required
+            type='text'
+            iconAndInputWrapperClassName='bg-white rounded-2xl h-16'
+            inputClassName='placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0  shadow-none'
           />
-          <CustomAuthInput label='Year' placeholder='2025' />
+          <AddInput
+            label='Year'
+            placeholder='2025'
+            id='vehicleYear'
+            errors={errors}
+            register={register}
+            disabled={false}
+            required
+            type='text'
+            iconAndInputWrapperClassName='bg-white rounded-2xl h-16'
+            inputClassName='placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0  shadow-none'
+          />
         </div>
       </div>
       <div className='flex flex-col gap-8 mt-5 text-center'>
@@ -108,9 +213,9 @@ const Page = () => {
       <AuthBackAndContinueButton
         backActive
         continueActive={true}
-        continueFnc={() => {
-          router.push("/onboarding/vehicle-insurance");
-        }}
+        continueFnc={handleSubmit(onSubmit)}
+        // router.push("/onboarding/vehicle-insurance");
+
         // continuePath='/onboarding/driver-info'
       />
     </div>
