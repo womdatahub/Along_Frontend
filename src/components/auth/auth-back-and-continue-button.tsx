a0f2b9436b93c887cc20642f97dcf1dd6c1365a9
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2026-01-27 09:26:37 +0100

    feat: enhance AuthBackAndContinueButton with loading state and integrate BtnLoader component

--- src/components/auth/auth-back-and-continue-button.tsx
+++ src/components/auth/auth-back-and-continue-button.tsx
@@ -2,17 +2,19 @@
 import { cn } from "@/lib";
 import { WhiteGreaterThanIcon } from "@public/svgs";
 import { useRouter } from "next/navigation";
-import { Button } from "@/components";
+import { BtnLoader, Button } from "@/components";
 
 type Props = {
   continueFnc: () => void;
   backActive: boolean;
   continueActive: boolean;
+  continueIsLoading?: boolean;
 };
 export const AuthBackAndContinueButton = ({
   continueActive,
   backActive,
   continueFnc,
+  continueIsLoading = false,
 }: Props) => {
   const router = useRouter();
   return (
@@ -26,7 +28,7 @@ export const AuthBackAndContinueButton = ({
         <div
           className={cn(
             "flex justify-center items-center rounded-full bg-primary w-14 aspect-square rotate-180",
-            backActive ? "bg-primary" : "bg-inactive"
+            backActive ? "bg-primary" : "bg-inactive",
           )}
         >
           <WhiteGreaterThanIcon />
@@ -39,17 +41,19 @@ export const AuthBackAndContinueButton = ({
         }}
         className={cn(
           "flex gap-3 items-center cursor-pointer bg-transparent text-black hover:bg-transparent border-0 shadow-none",
-          !continueActive && "cursor-not-allowed"
+          !continueActive && "cursor-not-allowed",
         )}
       >
         <p className='text-sm'>Continue</p>
         <div
           className={cn(
             "flex justify-center items-center rounded-full w-14 aspect-square transition-colors duration-500 bg-primary",
-            continueActive ? "bg-primary" : "bg-inactive"
+            continueActive ? "bg-primary" : "bg-primary/50",
           )}
         >
-          <WhiteGreaterThanIcon />
+          <BtnLoader isLoading={continueIsLoading} className='text-white'>
+            <WhiteGreaterThanIcon />
+          </BtnLoader>
         </div>
       </Button>
     </div>
--- src/components/shared/btn-loader.tsx
+++ src/components/shared/btn-loader.tsx
@@ -1,9 +1,15 @@
+import { cn } from "@/lib";
 import { Loader2Icon } from "lucide-react";
 
 type Props = {
   isLoading: boolean;
   children: React.ReactNode;
+  className?: string;
 };
-export const BtnLoader = ({ isLoading, children }: Props) => {
-  return isLoading ? <Loader2Icon className='animate-spin' /> : children;
+export const BtnLoader = ({ isLoading, children, className }: Props) => {
+  return isLoading ? (
+    <Loader2Icon className={cn("animate-spin", className)} />
+  ) : (
+    children
+  );
 };
