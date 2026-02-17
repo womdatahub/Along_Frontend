aebe4b6cd605e14abecb0813827090c1df7fed60
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2026-02-17 20:32:20 +0100

    chore next js config for the images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com/**",
+      },
+    ],
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
