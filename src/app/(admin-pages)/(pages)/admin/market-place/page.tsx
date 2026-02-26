b29314b0e498f9d1c7c651a26e66767e5e424ef5
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2026-02-26 21:00:11 +0100

    chore: refactor Market Place page to integrate AddOrEditNewFareEngineProfileComponent and clean up unused code

--- src/app/(admin-pages)/(pages)/admin/market-place/page.tsx
+++ src/app/(admin-pages)/(pages)/admin/market-place/page.tsx
@@ -20,7 +20,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { TMarketPlaceSchema, marketPlaceSchema } from "@/lib";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -28,31 +28,31 @@ import { useForm } from "react-hook-form";
 
 const isEmpty = true;
 const Page = () => {
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
 
-  const {
-    register,
-    setValue,
-    watch,
-    // handleSubmit,
-    formState: { errors },
-  } = useForm<TMarketPlaceSchema>({
-    defaultValues: {
-      platformFee: "",
-      currency: "",
-      baseFare: "",
-      driverToRiderFee: "",
-      waitChargePerMin: "",
-      tax: "",
-      baseHaggle: "",
-      maxHaggle: "",
-      surgeMultiplier: "",
-      profileName: "",
-    },
-    resolver: zodResolver(marketPlaceSchema),
-  });
+  // const {
+  //   register,
+  //   setValue,
+  //   watch,
+  //   // handleSubmit,
+  //   formState: { errors },
+  // } = useForm<TMarketPlaceSchema>({
+  //   defaultValues: {
+  //     platformFee: "",
+  //     currency: "",
+  //     baseFare: "",
+  //     driverToRiderFee: "",
+  //     waitChargePerMin: "",
+  //     tax: "",
+  //     baseHaggle: "",
+  //     maxHaggle: "",
+  //     surgeMultiplier: "",
+  //     profileName: "",
+  //   },
+  //   resolver: zodResolver(marketPlaceSchema),
+  // });
 
-  const email = watch("currency");
+  // const email = watch("currency");
   return (
     <section className='flex flex-col gap-8'>
       <p className='text-4xl font-heebo'>Market Place</p>
@@ -62,7 +62,7 @@ const Page = () => {
           <div className='flex justify-between gap-5 items-center px-6'>
             <p className='text-xl font-medium'>Fare Engine Profile</p>
             <div className='flex items-center gap-5'>
-              <Dialog open={open} onOpenChange={setOpen}>
+              {/* <Dialog open={open} onOpenChange={setOpen}>
                 <DialogTrigger asChild>
                   <Button className='rounded-full'>Add New</Button>
                 </DialogTrigger>
@@ -219,7 +219,11 @@ const Page = () => {
                     <Button className='px-14'>Save</Button>
                   </div>
                 </DialogContent>
-              </Dialog>
+              </Dialog> */}
+              <AddOrEditNewFareEngineProfileComponent
+                trigger={<Button>Add New</Button>}
+                isEdit
+              />
               <Button variant={"ghost"} className='rounded-full'>
                 Batch Delete
               </Button>
@@ -392,3 +396,191 @@ type Alert = {
 };
 
 const alertTables: Alert[] = [];
+
+type AddOrEditNewFareEngineProfileComponentType = {
+  trigger: ReactNode;
+  defaultValues?: TMarketPlaceSchema;
+  isEdit?: boolean;
+};
+const AddOrEditNewFareEngineProfileComponent = ({
+  trigger,
+  defaultValues,
+  isEdit,
+}: AddOrEditNewFareEngineProfileComponentType) => {
+  const [open, setOpen] = useState(false);
+
+  const {
+    register,
+    setValue,
+    watch,
+    // handleSubmit,
+    formState: { errors },
+  } = useForm<TMarketPlaceSchema>({
+    defaultValues,
+    resolver: zodResolver(marketPlaceSchema),
+  });
+  const currency = watch("currency");
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent
+        className='sm:max-w-[425px] md:max-w-[520px] px-4 py-8 rounded-[20px]'
+        showCloseButton={false}
+      >
+        <VisuallyHidden>
+          <DialogTitle>
+            {isEdit ? "Edit" : "Add new"} Fare Engine Profile
+          </DialogTitle>
+        </VisuallyHidden>
+        <div className='flex flex-col gap-6'>
+          <p className='-5 font-bold text-2xl'>
+            {isEdit ? "Edit" : "New"} Profile
+          </p>
+          <AddInput
+            label='Profile name'
+            id='profileName'
+            errors={errors}
+            placeholder='Rush Hour 22'
+            register={register}
+            required
+            type='text'
+            labelClassName='text-sm font-semibold ml-2'
+            iconAndInputWrapperClassName='bg-background-1 rounded-2xl p-0'
+            inputClassName='h-14 placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0 shadow-none'
+          />
+          <SelectDropdown
+            options={["USD", "NGN"]}
+            selected={currency}
+            setSelected={(value: string) => {
+              setValue("currency", value);
+            }}
+            triggerLabel='USD'
+            triggerClassName='bg-background-1 min-h-14 h-12'
+            labelClassName='ml-2'
+            label='Currency'
+            groupClassName='shadow-lg'
+            errorMessage={errors.currency?.message ?? ""}
+          />
+          <div className='flex gap-4'>
+            <AddInput
+              label='Base Fare'
+              id='baseFare'
+              errors={errors}
+              placeholder='0'
+              register={register}
+              required
+              type='text'
+              inputMode='numeric'
+              pattern='[0-9]*'
+              labelClassName='text-xs font-medium ml-2'
+              iconAndInputWrapperClassName='bg-background-1 rounded-lg flex-1 px-0'
+              inputClassName='h-12 placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0 shadow-none'
+            />
+            <AddInput
+              label='Driver to rider fee'
+              id='driverToRiderFee'
+              errors={errors}
+              placeholder='0'
+              register={register}
+              required
+              type='text'
+              inputMode='numeric'
+              pattern='[0-9]*'
+              labelClassName='text-xs font-medium ml-2'
+              iconAndInputWrapperClassName='bg-background-1 rounded-lg flex-1 px-0'
+              inputClassName='h-12 placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0 shadow-none'
+            />
+            <AddInput
+              label='Wait charge/min'
+              id='waitChargePerMin'
+              errors={errors}
+              placeholder='0'
+              register={register}
+              required
+              type='text'
+              inputMode='numeric'
+              pattern='[0-9]*'
+              labelClassName='text-xs font-medium ml-0'
+              iconAndInputWrapperClassName='bg-background-1 rounded-lg flex-1 px-0'
+              inputClassName='h-12 placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0 shadow-none'
+            />
+            <AddInput
+              label='Tax %'
+              id='tax'
+              errors={errors}
+              placeholder='0'
+              register={register}
+              required
+              type='text'
+              inputMode='numeric'
+              pattern='[0-9]*'
+              labelClassName='text-xs font-medium ml-0'
+              iconAndInputWrapperClassName='bg-background-1 rounded-lg flex-1 px-0'
+              inputClassName='h-12 placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0 shadow-none'
+            />
+          </div>
+          <div className='flex gap-4'>
+            <AddInput
+              label='Base Haggle %'
+              id='baseHaggle'
+              errors={errors}
+              placeholder='0'
+              register={register}
+              required
+              type='text'
+              inputMode='numeric'
+              pattern='[0-9]*'
+              labelClassName='text-xs font-medium ml-0'
+              iconAndInputWrapperClassName='bg-background-1 rounded-lg flex-1 px-0'
+              inputClassName='h-12 placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0 shadow-none'
+            />
+            <AddInput
+              label='Max Haggle %'
+              id='maxHaggle'
+              errors={errors}
+              placeholder='0'
+              register={register}
+              required
+              type='text'
+              inputMode='numeric'
+              pattern='[0-9]*'
+              labelClassName='text-xs font-medium ml-0'
+              iconAndInputWrapperClassName='bg-background-1 rounded-lg flex-1 px-0'
+              inputClassName='h-12 placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0 shadow-none'
+            />
+            <AddInput
+              label='Platform fee'
+              id='platformFee'
+              errors={errors}
+              placeholder='0'
+              register={register}
+              required
+              type='text'
+              inputMode='numeric'
+              pattern='[0-9]*'
+              labelClassName='text-xs font-medium ml-0'
+              iconAndInputWrapperClassName='bg-background-1 rounded-lg flex-1 px-0'
+              inputClassName='h-12 placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0 shadow-none'
+            />
+            <AddInput
+              label='Surge multiplier'
+              id='surgeMultiplier'
+              errors={errors}
+              placeholder='0'
+              register={register}
+              required
+              type='text'
+              inputMode='numeric'
+              pattern='[0-9]*'
+              labelClassName='text-xs font-medium ml-0'
+              iconAndInputWrapperClassName='bg-background-1 rounded-lg flex-1 px-0'
+              inputClassName='h-12 placeholder:text-placeholder text-sm font-medium font-fustat focus:outline-none focus:ring-0 border-0 shadow-none'
+            />
+          </div>
+          <Button className='px-14'>{isEdit ? "Edit" : "Save"}</Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
