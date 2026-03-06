61396c146eebb0edcc51bf42558ed53f87524ceb
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2026-03-06 08:36:01 +0100

    chore: admin store

---
+++ src/store/use-admin.ts
@@ -0,0 +1,63 @@
+import { create } from "zustand";
+import type { SelectorFn } from "@/types";
+import { devtools, persist } from "zustand/middleware";
+import { callApi, paymentApiStr } from "@/lib";
+import { toast } from "sonner";
+
+type AdminType = {
+    actions: {
+        createRideCostSettings: () => Promise<void>;
+        getRideCostSettings: () => Promise<void>;
+    },
+};
+
+const initialState = {
+
+};
+
+export const useAdmin = create<AdminType>()(
+    devtools(
+        persist(
+            (set) => ({
+                ...initialState,
+                actions: {
+                    createRideCostSettings: async () => {
+                        const { data, error } = await callApi(paymentApiStr('/cost/settings'), {})
+                        if (error) {
+                            toast.error(error.message)
+                            return
+                        }
+                        if (data) {
+                            toast.success(data.message ?? "Cost settings created successfully")
+                        }
+                    },
+                    getRideCostSettings: async () => {
+                        const { data, error } = await callApi(paymentApiStr('/cost/settings'))
+                        if (error) {
+                            toast.error(error.message)
+                            return
+                        }
+                        if (data) {
+                            toast.success(data.message ?? "Cost settings fetched successfully")
+                        }
+                    },
+
+                },
+            }),
+            {
+                name: "use-admin-store",
+                partialize: (state) => ({
+
+                }),
+            },
+        ),
+    ),
+);
+
+export const useAdmins = <TResult>(
+    selector: SelectorFn<AdminType, TResult>,
+) => {
+    const state = useAdmin(selector);
+
+    return state;
+};
