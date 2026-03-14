5e3363885d76bf0e207ac4b46d95b6802e6d7500
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2026-03-14 21:50:59 +0100

    chore: preventing drivers and admins from accessing this route

--- src/app/rent-ride/layout.tsx
+++ src/app/rent-ride/layout.tsx
@@ -1,11 +1,30 @@
 "use client";
 import { Navbar } from "@/components";
+import { ROLE_DASHBOARD_MAP } from "@/lib";
+import { useSession } from "@/store";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useShallow } from "zustand/shallow";
 
 const Layout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const { userRole } = useSession(
+    useShallow((state) => ({
+      userRole: state.userRole,
+    })),
+  );
+  const router = useRouter();
+
+  if (userRole === "driver" || userRole === "admin") {
+    toast.error(
+      "You are not authorized to access this page. Please log in as a rider.",
+    );
+    router.push(ROLE_DASHBOARD_MAP[userRole]);
+    return;
+  }
   return (
     <section className='w-screen min-h-screen flex flex-col bg-background-1'>
       <Navbar />
